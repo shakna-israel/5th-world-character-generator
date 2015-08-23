f20e185a94a3b3e55d7498d7904e23171e5443f7
James Milne <s4b3r6@gmail.com>
2015-08-23 21:41:30 +1000

    Initial Structure

---
+++ pygen.py
@@ -0,0 +1,17 @@
+def choose_name(sex):
+    raise NotImplementedError
+
+def choose_profession():
+    raise NotImplementedError
+
+def choose_age(minAge, maxAge):
+    raise NotImplementedError
+
+def choose_sex():
+    raise NotImplementedError
+
+def main():
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    main()

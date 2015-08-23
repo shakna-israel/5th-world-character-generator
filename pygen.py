41ef0b574a7753b26234d48c30bf37024464001f
James Milne <s4b3r6@gmail.com>
2015-08-23 21:47:04 +1000

    Implemented choose_sex

--- pygen.py
+++ pygen.py
@@ -1,5 +1,11 @@
 def choose_name(sex):
-    raise NotImplementedError
+    if sex == "male":
+        return NotImplementedError
+    elif sex == "female":
+        return NotImplementedError
+    else:
+        print("Sex not male nor female.")
+        raise TypeError
 
 def choose_profession():
     raise NotImplementedError
@@ -8,9 +14,17 @@ def choose_age(minAge, maxAge):
     raise NotImplementedError
 
 def choose_sex():
-    raise NotImplementedError
+    chance = random.randint(0,1)
+    if chance = 1:
+        return "male"
+    else:
+        return "female"
 
 def main():
+    charSex = choose_sex()
+    charName = choose_name(charSex)
+    charProfession = choose_profession()
+    charAge = choose_age(random.randint(14,60),random.randint(14,60))
     raise NotImplementedError
 
 if __name__ == "__main__":
